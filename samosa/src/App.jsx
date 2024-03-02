bb73736ec7acde5ed17b1d0d581efe4c7b2959c5
albert-reyes <109039459+albert-reyes@users.noreply.github.com>
2024-03-02 11:44:37 -0800

    firee

---
+++ samosa/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [count, setCount] = useState(0)
+
+function handleCount(){
+  setCount(count + 1);
+}
+
+  return (
+    <>
+      <h1>Count: {count}</h1>
+      <button onClick={handleCount}>Set count</button>
+    </>
+
+  )
+}
+
+export default App
